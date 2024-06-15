2fff6677e2bbab79b6d51405f354a360a377d181
VishnuGurrapu <gurrapuvishnu20@gmail.com>
2024-06-15 19:10:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const addDays = require("date-fns/addDays");
-const getDateAfterXDays = (days) ⇒ {
+const getDateAfterXDays = (days) => {
 const newDate = addDays (new Date(2020, 7, 22), days);
 return `${newDate.getDate()}-${
 newDate.getMonth() + 1
